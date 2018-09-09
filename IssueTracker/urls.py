063aa7f6ed7c6f4662f009d08fcda158a4c9f2e5
Marc Marquez <marc.marquez@gmail.com>
2018-09-09 14:51:43 -0700

    First commit.

---
+++ IssueTracker/urls.py
@@ -0,0 +1,30 @@
+"""IssueTracker URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url, include
+from main import views as main_views
+from accounts import views as accounts_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^$', main_views.get_index, name='index'),
+    url(r'^register/$', accounts_views.register, name='register'),
+    url(r'^profile/$', accounts_views.profile, name='profile'),
+    url(r'^login/$', accounts_views.login, name='login'),
+    url(r'^logout/$', accounts_views.logout, name='logout'),
+    url(r'^pages/', include('django.contrib.flatpages.urls')),
+]
---
+++ main/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def get_index(request):
+    return render(request, 'index.html')
