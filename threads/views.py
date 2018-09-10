95e70388e29245b513c0aff61c9e5b5ed7666798
Marc Marquez <marc.marquez@gmail.com>
2018-09-09 17:13:40 -0700

    Added font-awesome

--- threads/views.py
+++ threads/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.template.context_processors import csrf
+from .forms import ThreadForm, PostForm
 
 
 def forum(request):
@@ -14,6 +15,36 @@ def threads(request, subject_id):
     subject = get_object_or_404(Subject, pk=subject_id)
     return render(request, 'forum/threads.html', {'subject': subject})
 
+
 @login_required
 def new_thread(request, subject_id):
-    pass
+    subject = get_object_or_404(Subject, pk=subject_id)
+    if request.method == "POST":
+        thread_form = ThreadForm(request.POST)
+        post_form = PostForm(request.POST)
+        if thread_form.is_valid() and post_form.is_valid():
+            thread = thread_form.save(False)
+            thread.subject = subject
+            thread.user = request.user
+            thread.save()
+
+            post = post_form.save(False)
+            post.user = request.user
+            post.thread = thread
+            post.save()
+
+            messages.success(request, "Your have create a new thread!")
+
+            return redirect(reverse('thread', args=[thread.pk]))
+    else:
+        thread_form = ThreadForm()
+        post_form = PostForm()
+
+    args = {
+        'thread_form': thread_form,
+        'post_form': post_form,
+        'subject': subject,
+    }
+    args.update(csrf(request))
+
+    return render(request, 'forum/thread_form.html', args)
