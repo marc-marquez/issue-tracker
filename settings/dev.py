a12b80ecdc15f1d445c8596b7503c9ee394efef3
Marc Marquez <marc.marquez@gmail.com>
2018-09-28 14:26:03 -0700

    Switched to gunicorn 19.5 because of Git vulnerability. Cleaned up dev.py and staging.py for the correct database instances.

--- settings/dev.py
+++ settings/dev.py
@@ -10,27 +10,6 @@ DATABASES = {
    }
 }
 
-#import dj_database_url
-
-#DATABASES = {}
-# Load the ClearDB connection details from the environment variable
-#CLEARDB_DATABASE_URL = os.environ.get('CLEARDB_DATABASE_URL')
-
-#parsed_url = dj_database_url.parse(CLEARDB_DATABASE_URL)
-#print(parsed_url)
-
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'heroku_41fd08de6cc6915',
-#        'USER': 'bad41e77edb428',
-#        'PASSWORD': '8bb00b3b',
-#        'HOST': 'us-cdbr-iron-east-01.cleardb.net',
-#        'PORT': None,
-#    }
-#}
-
 # Stripe environment variables
 STRIPE_PUBLISHABLE = os.environ.get('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.environ.get('STRIPE_SECRET')
--- settings/staging.py
+++ settings/staging.py
@@ -4,13 +4,6 @@ import dj_database_url
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 CLEARDB_DATABASE_URL = os.environ.get('CLEARDB_DATABASE_URL')
 
 parsed_url = dj_database_url.parse(CLEARDB_DATABASE_URL)
