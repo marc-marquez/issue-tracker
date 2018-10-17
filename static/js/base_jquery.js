f8747cebe28356357942fb930e601a0f632d061f
Marc Marquez <marc.marquez@gmail.com>
2018-10-16 22:07:32 -0700

    Added jumbotron image to profile page. Switched colors of upvote button for less confusion. Added Back to Ticket button for ticket page.

--- static/js/base_jquery.js
+++ static/js/base_jquery.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
-});
-
-/*$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
 });*/
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
 
 $(document).ready(function() {
   jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 5000  // 5 seconds
