112b4e2b5b1624bf37e43c33650859e5f432ff85
Marc Marquez <marc.marquez@gmail.com>
2018-11-29 12:05:50 -0800

    Added access to users in admin site.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ('username','email','is_staff','is_superuser','is_active')
 
 # Register your models here.
+admin.site.register(User,UserAdmin)
