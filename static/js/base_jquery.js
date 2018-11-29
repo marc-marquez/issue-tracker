d9554286c5dbfb3e3b59f7a8f7c861af40a96edf
Marc Marquez <marc.marquez@gmail.com>
2018-11-28 23:28:30 -0800

    Changed from bootstrap-table to datatable for better sorting.

--- static/js/base_jquery.js
+++ static/js/base_jquery.js
@@ -1,5 +1,23 @@
 /* Enables all tooltips */
 $(document).ready(function(){
+    $('#ticketTable').DataTable({
+        'autoWidth':true,
+        'responsive':true,
+        'columnDefs': [
+            {responsivePriority: 3, targets: 0},
+            {responsivePriority: 1, targets: 1},
+            {'width': '30%', targets: 1},
+            {responsivePriority: 6, targets: 2},
+            {responsivePriority: 7, targets: 3},
+            {responsivePriority: 5, targets: 4},
+            {responsivePriority: 2, targets: 5},
+            {responsivePriority: 4, targets: 6},
+            {'width': '5%', targets: 6},
+        ],
+        drawCallback: function () {
+            $('[data-toggle="tooltip"]').tooltip();
+        }
+    });
     $('[data-toggle="tooltip"]').tooltip();
 });
 
